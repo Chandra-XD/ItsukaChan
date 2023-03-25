b6bd94470dd37636a92ed668d6746eaddbf97e85
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-25 22:29:26 +0700

    -

--- plugins/_autoblock.js
+++ plugins/_autoblock.js
@@ -6,7 +6,7 @@ handler.before = async function (m) {
     await conn.updateBlockStatus(m.sender, "block")
    }
    
-   if (m.sender.startsWith('265' || '265')) {
+   if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
     await conn.updateBlockStatus(m.sender, "block")
    }
