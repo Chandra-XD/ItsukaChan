56964af791ec1befb625def57a6e306df8e5d721
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-26 12:43:17 +0700

    -

--- plugins/anonymous_chat_.js.js
+++ plugins/anonymous_chat_.js.js
@@ -2,7 +2,7 @@ export async function before(m, { match }) {
     // if (match) return !1
     if (!m.chat.endsWith('@s.whatsapp.net'))
         return !0
-    this.anonymous = this.anonymous ? this.anonymous : {}
+    conn.anonymous = conn.anonymous ? conn.anonymous : {}
     let room = Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender) && room.state === 'CHATTING')
     if (room) {
         if (/^.*(next|leave|start)/.test(m.text))
