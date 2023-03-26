7a0b26dcf5ba88267c99ae743ad4073b33690c61
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-26 12:20:54 +0700

    -

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,12 +2,9 @@ let handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`
-${conn.getName(m.sender)} sekarang telah AFK dengan alasan ${text ? ': ' + text : ''}
-`)
+  m.reply(`${conn.getName(m.sender)} sekarang telah AFK dengan alasan ${text ? ': ' + text : ''}`)
 }
-handler.help = ['afk [alasan]']
+handler.help = ['afk'].map(v => v + ' <alasan>')
 handler.tags = ['main']
 handler.command = /^afk$/i
-
 export default handler
