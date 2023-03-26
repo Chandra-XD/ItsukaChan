7d3e9500a205c27a1b4ac2daede596eb21b370d3
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-26 12:41:59 +0700

    -

--- plugins/anonymous-sendkontak.js
+++ plugins/anonymous-sendkontak.js
@@ -1,6 +1,6 @@
 let { MessageType, Presence } = (await import('@adiwajshing/baileys')).default
 async function handler(m, { command, conn, text }) {
-	this.anonymous = this.anonymous ? this.anonymous : {}
+	conn.anonymous = conn.anonymous ? conn.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
 	if (!room) throw 'kamu tidak berada di anonymous chat'
@@ -11,8 +11,8 @@ async function handler(m, { command, conn, text }) {
 	var number = who.split('@')[0]
 	let tks = `➔ Nomor: ${m.sender.split`@`[0]}
 ➔ Nama: ${name}`
-        conn.reply(m.chat, 'Menggirimkan Kontak...')
-	if (other) conn.reply(other, `Partner mengirimkan kontak kepadamu`)
+        conn.reply(m.chat, 'Menggirimkan Kontak...', null)
+	if (other) conn.reply(other, `Partner mengirimkan kontak kepadamu`, null)
 	if (other) conn.sendMessage(other, { image: { url: await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png')}, caption: tks }, { quoted: m })
 }
 handler.help = ['sendkontak']
