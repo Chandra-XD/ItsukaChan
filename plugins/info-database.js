5f0777762241b5f0b15c1870a8cd6d62dee89545
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-26 11:50:34 +0700

    -

--- plugins/info-database.js
+++ plugins/info-database.js
@@ -1,7 +1,9 @@
 let handler = async (m) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.reply(`*Jumlah database saat ini ${totalreg} user*`)
+    m.reply(`*Jumlah database saat ini ${totalreg} user*\n*Dan user teregistrasi saat ini ${rtotalreg} user*`)
 }
+handler.help = ['database']
+handler.tags = ['info']
 handler.command = /^(database|jumlahdatabase|user)$/i
 export default handler
