bca56ff91770171c2f67f90833844c077b513d83
Muhammad Candra <115627115+Tjandraaaa@users.noreply.github.com>
2023-03-26 19:13:28 +0700

    -

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -1,30 +1,19 @@
 import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
-	let fdoc = {
-  key : {
-  remoteJid: 'status@broadcast',
-  participant : '0@s.whatsapp.net'
-  },
-  message: {
-  documentMessage: {
-  title: '©Sad-Bot ', 
-  jpegThumbnail: fs.readFileSync('./thumbnail.jpg'),
-                            }
-                          }
-                        }
+import { apivisit } from './kanghit.js'
+
+let handler = async (m, { conn }) => {
 	let d = new Date
             let date = d.toLocaleDateString('id', {
                 day: 'numeric',
                 month: 'long',
                 year: 'numeric'
             })
-conn.reply(m.chat, '*Succes*', m)
-conn.reply('17608914335' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
-          conn.sendFile('17608914335'+ '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
+let a = await conn.reply(global.owner[0] + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
+conn.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json', quoted: a})
+await apivisit
  }
  
- handler.help = ['backup']
+handler.help = ['backup']
 handler.tags = ['owner']
 handler.command = /^(b|backup)$/i
 
