aa785bc0e2652d06a4b3986354c95c61c12403c6
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-26 12:17:41 +0700

    fix button :v

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -11,8 +11,8 @@ export async function before(m, { isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_'}`, author, ['off antilink', '/disable antilink'], m)
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        await conn.reply(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_'}`, m)
+        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 
     }
     return !0
