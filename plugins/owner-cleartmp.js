d0fadbb0d43762c19db6dc894e4425b19fef943a
Muhammad Candra <115627115+Tjandraaaa@users.noreply.github.com>
2023-03-28 01:59:09 +0700

    -

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,9 +1,16 @@
-import { join, path } from 'path'
-import {readdirSync,statSync,unlinkSync,existsSync,readFileSync,watch} from 'fs'
-let handler = async (m, { args, text }) => {
-conn.sendMessage(m.chat, {react : { text : '🚮', key : m.key}})
-conn.reply(m.chat, 'Udah can 😘', m)
-const tmp = [join(__dirname, '../tmp')]
+import { tmpdir } from 'os'
+import path, { join } from 'path'
+import {
+  readdirSync,
+  statSync,
+  unlinkSync,
+  existsSync,
+  readFileSync,
+  watch
+} from 'fs'
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+m.reply('Succes !')
+const tmp = [tmpdir(), join(__dirname, '../tmp')]
   const filename = []
   tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
   return filename.map(file => {
@@ -13,6 +20,6 @@ const tmp = [join(__dirname, '../tmp')]
 }
 handler.help = ['cleartmp']
 handler.tags = ['owner']
-handler.command = /^(ctm)$/i
-handler.owner = true
+handler.command = /^(cleartmp)$/i
+handler.rowner = true
 export default handler
