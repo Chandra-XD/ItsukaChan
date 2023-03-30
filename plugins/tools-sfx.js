82738de297eafd65e7391612c71da1c3be344daf
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-31 06:15:38 +0700

    -

---
+++ plugins/tools-sfx.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, usedPrefix, text, args, command }) => {
+if (!text) throw `Contoh:
+${usedPrefix + command} 10`
+let json = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
+        let jsons = await json.json()
+        let caption = '*⎔┉━「 Search 」━┉⎔*'
+        for (let x of jsons.results) {
+        caption += `
+        *Name :* ${x.name}
+*Sound :* ${x.sound}
+`}
+        return m.reply(caption)
+}
+
+handler.command = handler.help = ['sfx']
+handler.tags = ['tools']
+export default handler
