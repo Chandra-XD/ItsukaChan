4df3e588c8777b8af4625c6082c53678b5947ce2
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-06 07:22:19 +0700

    nih dek, biar bisa acc

--- plugins/misc-join.js
+++ plugins/misc-join.js
@@ -1,21 +1,28 @@
-let linkRegex = /([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
-let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
-
-    let [_, code, expired] = text.match(linkRegex) || []
+let handler = async (m, { conn, text, isOwner }) => {
+    let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
+    if (!isOwner) {
+       m.reply('Link grup telah dikirimkan ke owner!\nHarap menunggu acc dari owner...')
+       for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+        let data = (await conn.onWhatsApp(jid))[0] || {}
+        // conn.copyNForward(data.jid, m, true)
+        conn.sendButton(data.jid, `@${m.sender.split('@')[0]} Ingin menambahkan Botmu ke ${text}\nTerima/Tolak?`, 'Join Acc', null, [['Terima', `.join ${text}`], ['Tolak', '.']], m, { mentions: [m.sender] })
+      }
+       return
+    }
     let res = await conn.groupAcceptInvite(code)
     if (!res) throw res.toString()
     let name = await conn.getName(res).catch(_ => null)
+    // expired =  Math.floor(Math.min(999, Math.max(1, isOwner ? expired && expired.isNumber() ? parseInt(expired) : 0 : 3)))
+    m.reply(`Berhasil join grup ${name || res}`)/*${expired ? `selama ${expired} hari` : ''}`)
     
-    let caption = `*Berhasil join grup* ${name || res}\n`
-  
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-
-return conn.sendButton(m.chat,caption, wm, 'Ok', 'y sj') 
+    */
 }
 handler.command = /^join$/i
-handler.premium = true
+// handler.rowner = true
 export default handler
