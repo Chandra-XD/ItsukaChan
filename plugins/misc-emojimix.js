c146dee45614f66b62e811d5008f9121945f4e05
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-06 07:25:28 +0700

    Add files via upload

--- plugins/misc-emojimix.js
+++ plugins/misc-emojimix.js
@@ -2,14 +2,15 @@ import fetch from 'node-fetch'
 import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	let [emoji1, emoji2] = text.split`+`
-	if (emoji1 && emoji2) {
-		let url = API('violetics', '/api/media/emojimix', { emoji1, emoji2 }, 'apikey')
-		let res = await fetch(url)
-		if (res.status !== 200) throw res.statusText
-		let stiker = await (new Sticker(url)).toMessage()
+	let [emo1, emo2] = text.split`+`
+	if (!(emo1 && emo2)) throw `Ex: ${usedPrefix+command} ${decodeURI('%F0%9F%92%80')}+${decodeURI('%F0%9F%92%80')}`
+	let url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${emo1}_${emo2}`
+	let res = await (await fetch(url)).json()
+	if (!res.results.length) throw 'An error occurred'
+	for (let x of res.results) {
+		let stiker = await (new Sticker(x.url, { type: 'full', categories: x.tags })).toMessage()
 		conn.sendMessage(m.chat, stiker, { quoted: m })
-	} else throw `Ex: ${usedPrefix+command} ${decodeURI('%F0%9F%92%80')}+${decodeURI('%F0%9F%92%80')}`
+	}
 }
 handler.help = ['emojimix']
 handler.tags = ['misc']
