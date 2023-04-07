24433f90ae5e6819f57a566d9bc44647efd17167
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-07 15:27:38 +0700

    apa cona

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,37 +1,62 @@
-let limit = 30
-import fetch from 'node-fetch'
-import { youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+let limit = 80
+import axios from 'axios'
+import yts from 'yt-search'
+import { validateURL, getVideoID } from '../lib/ytUrlUtils.js'
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import { apivisit } from './kanghit.js'
 
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-if (args && /(?:https?:\/{2})?(?:w{3}|m|music)?\.?youtu(?:be)?\.(?:com|be)(?:watch\?v=|\/)([^\s&]+)/i.test(args[0])) {
-let qu = args[2] || '360'
-let q = qu + 'p'
-let v = args[0]
-let res = await fetch(`https://ytdl.pnggilajacn.my.id/yt?url=${args[0]}`)
-res = await res.json()
-if (!res) res = ''
-let { description, ownerChannelName, viewCount, uploadDate, likes, dislikes } = res.result.videoDetails
-let yt = await youtubedlv2(v).catch(async _ => await youtubedlv3(v))
-let dl_url = await yt.video[q].download()
-let ttl = await yt.title
-let size = await yt.video[q].fileSizeH
-let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < size
-let _thumb = {}
-try { _thumb = { jpegThumbnail: await (await fetch(yt.thumbnail)).buffer() } }
-catch (e) { }
-await m.reply('Sedang diproses...')
-let repl = await conn.sendMessage(m.chat, { [/^(?:-|--)doc$/i.test(args[1]) || isLimit ? 'document' : 'video']: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', ..._thumb }, { quoted: m })
-let caption = `*Title:* ${ttl}\n*Quality:* ${q}\n*Channel:* ${ownerChannelName || ''}\n*Views:* ${viewCount}\n*Upload Date:* ${uploadDate}${likes ? `\n*Likes:* ${likes}` : ''}${dislikes ? `\n*Dislikes*: ${dislikes}` : ''}${description ? `\n*Description:*\n${description}` : ''}`.trim()
-await conn.sendMessage(m.chat, { image: { url: yt.thumbnail }, caption }, { quoted: repl })
-await apivisit
-  } else throw 'Invalid URL'
-  	// By Chandra XD
-	// Follow bang
-	// TikTok : @pnggilajacn
-	// Github : https://github.com/Chandra-XD
+	// let key = 'AIzaSyA2-LY3jRpNm1ycJ_ribbSOvOr99wMQQqA'
+	if (validateURL(args[0])) {
+		let id = await getVideoID(args[0]), vid = await yts({ videoId: id }), opt = args[1] && args[1].isNumber() ? args[1].replace(/\D/g, '') : ''
+		let { description: desc, timestamp, views, uploadDate, ago, author: { name }} = vid
+		let { thumbnail, video: _video, title } = await youtubedlv2(args[0]).catch(async _ => await youtubedl(args[0])).catch(async _ => await youtubedlv3(args[0]))
+		await m.reply('Sedang diproses...')
+		let limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
+		let video, quality, link, lastError, isLimit // source 
+		for (let i in _video) {
+			try {
+				video = _video[i]
+				quality = video.quality
+				console.log(video)
+				isLimit = video.fileSize > limitedSize
+				if (isLimit && /1080p/.test(quality) || !quality.includes(opt)) continue 
+				link = await video.download()
+				if (link) break 
+			} catch (e) {
+				video = quality = link = null
+				lastError = e
+				continue 
+			}
+		}
+		if (!link) throw 'Error: ' + (lastError || 'Can\'t download video')
+		let _thumb = {}
+		try { _thumb = { jpegThumbnail: (await conn.getFile(thumbnail)).data } }
+		catch (e) { }
+		await conn.sendMessage(m.chat, { [/^(?:-|--)doc$/i.test(args[1]) || isLimit ? 'document' : 'video']: { url: link }, fileName: `${title}.mp4`, mimetype: 'video/mp4', ..._thumb }, { quoted: m }).then(async (msg) => {
+			let caption = `*Title:* ${title}\n*Quality:* ${quality}\n*Channel:* ${name}\n*Duration:* ${timestamp}\n`
+				+ `*Upload Date:* ${uploadDate}\n*Views:* ${views}${desc ? `\n*Description:*\n${desc}` : ''}`.trim()
+			await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption }, { quoted: msg })
+		})
+		await apivisit
+	} else throw 'Invalid URL'
 }
 handler.help = ['mp4'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 export default handler
+
+function convertTime(num) {
+	num = num.match(/(\d+)(?=[HMS])/gi) || []
+	num = num.map((v) => {
+		if (v.length < 2) return '0' + v
+		return v
+	}).join(':')//.padStart(5, '00:0')
+	return num
+}
+
+async function shortUrl(url) {
+	url = encodeURIComponent(url)
+	let res = await axios.get(`https://is.gd/create.php?format=simple&url=${url}`)
+	return res.data
+}
