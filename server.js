85443f2764a79cc3b29175047a0052e83f5e26c7
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-26 16:18:32 +0700

    -

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ function connect(PORT) {
 	
 	app.get('/nowa', async (req, res) => {
 		let q = req.query.number, regex = /x/g
-		if (!q) return res.send('Input Parameter Number Parameter')
-		if (!q.match(regex)) return res.send('Parameter Number Must Fill With One Letter "x"')
+		if (!q) return res.json('Input Parameter Number Parameter')
+		if (!q.match(regex)) return res.json('Parameter Number Must Fill With One Letter "x"')
 		let random = q.match(regex).length, total = Math.pow(10, random), array = []
 		for (let i = 0; i < total; i++) {
 			let list = [...i.toString().padStart(random, '0')]
