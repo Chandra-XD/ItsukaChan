237560d1298f94be64caac00d824d367c74c700a
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-27 10:56:52 +0700

    ^

--- plugins/downloader-yts.js
+++ plugins/downloader-yts.js
@@ -3,13 +3,19 @@ import { apivisit } from './kanghit.js'
 
 let handler = async (m, { conn, text, usedPrefix: _p }) => {
 	if (!text) throw 'Query??'
-	let res = (await yts(text)).videos 
+/*	let res = (await yts(text)).videos 
 	if (!res.length) throw `Query "${text}" Not Found`
 	let arr = [], arr2 = []
-	for (let x of res) arr.push({ title: x.title, description: `Uploaded ${x.ago || '-'}, ${parseInt(x.views).toLocaleString()} views`, rowId: `${_p}yta ${x.url}` })
+	for (let x of res) arr.push({ title: x.title, description: `Uploaded ${x.ago}, ${parseInt(x.views).toLocaleString()} views`, rowId: `${_p}yta ${x.url}` })
 	for (let x of res) arr2.push({ title: x.title, description: `Uploaded ${x.ago}, ${parseInt(x.views).toLocaleString()} views`, rowId: `${_p}ytv ${x.url}` })
-	await conn.sendMessage(m.chat, { text: `Result From: ${text}`, footer: null, title: null, buttonText: 'Result', sections: [{ title: 'Audio', rows: arr }, { title: 'Video', rows: arr2 }] }, { quoted: m })
-	await apivisit
+	await conn.sendMessage(m.chat, { text: `Result From: ${text}`, footer: null, title: null, buttonText: 'Result', sections: [{ title: 'Audio', rows: arr }, { title: 'Video', rows: arr2 }] }, { quoted: m })*/
+  let results = await yts(text)
+  let tes = results.all
+  let teks = results.all.map(v => {
+  switch (v.type) {
+  case 'video': return `*_${v.title}_*\n_${v.url}_\n_Uploaded ${v.ago}, ${parseInt(v.views).toLocaleString()} views_`}}).filter(v => v).join('\n\n')
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+  await apivisit
 }
 handler.help = ['earch'].map(v => 'yts' + v + ' <query>')
 handler.tags = ['tools']
