ff5f78f77cdccfe75aa6357ee9f85f33c2a9cef5
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-27 10:59:57 +0700

    ^

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,17 +1,59 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, args }) => {
-   if (!args[0]) throw 'Nothing url'
-   if (!/^https?:\/\//.test(args[0])) throw 'Param *URL* must be starts with http:// or https://'
-   try {
-   let url = API('can', '/api/other/ssweb', { link: args[0] })
-   conn.sendMessage(m.chat, { image: { url }, caption: `${args[0]}` }, { quoted: m })
-   } catch (e) {
-		console.log(e)
-		m.reply(`Maaf erjadi kesalahan.`)
-	}
+import axios from 'axios'
+let handler = async (m, { conn, text, command }) => {
+if (!text) throw 'Nothing url'
+if (!/^https?:\/\//.test(text)) throw 'Param *URL* must be starts with http:// or https://'
+var phone = await ssweb(text, 'phone')
+var desktop = await ssweb(text, 'desktop')
+var tablet = await ssweb(text, 'tablet')
+var res = ``
+if (command === 'sshp') {
+await conn.sendFile(m.chat, phone.result, '',res, m, false)
 }
-handler.help = ['ssweb']
+if (command === 'ssweb' || command === 'sstablet') {
+await conn.sendFile(m.chat, tablet.result, '',res, m, false)
+}
+if (command === 'sspc') {
+await conn.sendFile(m.chat, desktop.result, '',res, m, false)
+}
+}
+handler.help = ['ssweb','sspc','sshp','sstablet']
 handler.tags = ['internet']
-handler.command = /^ss(web)?f?$/i
+handler.command = /^(ssweb|sstablet|sspc|sshp)$/i
 export default handler
+
+async function ssweb(url, device = 'desktop'){
+     return new Promise((resolve, reject) => {
+          const base = 'https://www.screenshotmachine.com'
+          const param = {
+            url: url,
+            device: device,
+            cacheLimit: 0
+          }
+          axios({url: base + '/capture.php',
+               method: 'POST',
+               data: new URLSearchParams(Object.entries(param)),
+               headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+               }
+          }).then((data) => {
+               const cookies = data.headers['set-cookie']
+               if (data.data.status == 'success') {
+                    axios.get(base + '/' + data.data.link, {
+                         headers: {
+                              'cookie': cookies.join('')
+                         },
+                         responseType: 'arraybuffer'
+                    }).then(({ data }) => {
+                       let result = {
+                            status: 200,
+                            author: 'Chandra-XD',
+                            result: data
+                        }
+                         resolve(result)
+                    })
+               } else {
+                    reject({ status: 404, author: 'Chandra-XD', message: data.data })
+               }
+          }).catch(reject)
+     })
+}
