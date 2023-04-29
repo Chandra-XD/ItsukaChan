6fdc96a0ba95a5679f24e500be387e803a4453b8
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-30 06:48:16 +0700

    :v

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -6,10 +6,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Input *URL*`
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_source=ig_web_copy_link`
     let api = await (await axios.get(`https://sh.xznsenpai.xyz/api/igdl?url=`+args[0])).data
-    let results = api.media[0]
+    let results = api.media[0] || api.media
 //  let results = (await instagramGetUrl(args[0])).url_list[0]
     await m.reply('Sedang diproses...')
-    await conn.sendFile(m.chat, results, '', wm, m)
+    await conn.sendFile(m.chat, results, 'instagram.mp4', wm, m)
     await apivisit
 }
 handler.help = ['instagram'].map(v => v + ' <url>')
