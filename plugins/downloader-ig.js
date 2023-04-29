3fa5ca4ec2f1bc73dc7653416a54c8d6650446a4
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-04-30 03:48:14 +0700

    ^

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,10 +1,13 @@
 import instagramGetUrl from 'fg-ig'
+import axios from 'axios'
 import { apivisit } from './kanghit.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_source=ig_web_copy_link`
+    if (!args[0]) throw `Input *URL*`
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_source=ig_web_copy_link`
-    let results = (await instagramGetUrl(args[0])).url_list[0]
+    let api = await (await axios.get(`https://sh.xznsenpai.xyz/api/igdl?url=`+args[0])).data
+    let results = api.media[0]
+//  let results = (await instagramGetUrl(args[0])).url_list[0]
     await m.reply('Sedang diproses...')
     await conn.sendFile(m.chat, results, '', wm, m)
     await apivisit
