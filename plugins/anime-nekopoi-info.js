24bbf6611e2bc7165234f6569aafd72a14c83038
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-06 07:19:23 +0700

    ^

--- plugins/anime-nekopoi-info.js
+++ plugins/anime-nekopoi-info.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 import { apivisit } from './kanghit.js'
 
-let handler = async (m, { conn, args }) => {
-    if (!args[0]) throw `Id?`
-    let res = (await axios.get(API('can', '/api/anime/nekopoi/detail', { id: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
+let handler = async (m, { conn, text, usedPrefix: _p }) => {
+    if (!text) throw `Id?`
+    let res = (await axios.get(API('can', '/api/anime/nekopoi/detail', { id: text } ))).data;
     try {
     let v = res.info_meta
 	let arr = []
-	for (let x of res.episode) arr.push({ title: x.title, description: `Upload : ${x.date || 'unknown'} || Id : ${x.id}`, rowId: `#nekodl ${x.id}` })
+	let tekss = res.episode.map(v => { return `${v.title}\nUpload : ${v.date || 'unknown'} || Id : ${v.id}`}).filter(v => v).join('\n\n')
+	for (let x of res.episode) arr.push({ title: x.title, description: `Upload : ${x.date || 'unknown'} || Id : ${x.id}`, rowId: `${_p}nekodl ${x.id}` })
+	if (m.isGroup) return m.reply(`*Title :* ${res.title}\n*Id :* ${res.id || '-'}\n*Date :* ${res.date ||'-'}\n*Japanese Title :* ${v.aliases || '-'}\n*Score :* ${v.skor || '-'}\n*Produser :* ${v.produser || '-'}\n*Status :* ${v.status || '-'}\n*Total Eps :* ${v.episode || '-'}\n*Durasi :* ${v.durasi || '-'}\n*Release :* ${v.tayang || '-'}\n*Genre :* ${v.genre || '-'}\n\n\n${tekss}`)
 	await conn.sendMessage(m.chat, { text: `*Title :* ${res.title}\n*Id :* ${res.id || '-'}\n*Date :* ${res.date ||'-'}\n*Japanese Title :* ${v.aliases || '-'}\n*Score :* ${v.skor || '-'}\n*Produser :* ${v.produser || '-'}\n*Status :* ${v.status || '-'}\n*Total Eps :* ${v.episode || '-'}\n*Durasi :* ${v.durasi || '-'}\n*Release :* ${v.tayang || '-'}\n*Genre :* ${v.genre || '-'}`, footer: null, title: null, buttonText: 'Click Here!', sections: [{ title: 'Nekopoi', rows: arr }] }, { quoted: m })
 	await apivisit
 	} catch {
