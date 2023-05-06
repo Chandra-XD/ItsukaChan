7be0a17cdb8ff6f30b0d36a81408b6d78ee97558
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-06 07:20:26 +0700

    ^

--- plugins/anime-nekopoi-latest.js
+++ plugins/anime-nekopoi-latest.js
@@ -1,14 +1,19 @@
 import axios from "axios"
 import { apivisit } from './kanghit.js'
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix: _p }) => {
     let res = (await axios.get(API('can', '/api/anime/nekopoi/latest'))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
 	let arr = []
-	for (let x of res) arr.push({ title: x.title, description: `Upload : ${x.date || 'unknown'} || Id : ${x.id}`, rowId: `#nekoinfo ${x.id}` })
-	conn.sendMessage(m.chat, { text: `Nekopoi Latest`, footer: null, title: null, buttonText: 'Click Here!', sections: [{ title: 'Nekopoi', rows: arr }] }, { quoted: m })
+	let tekss = res.map(v => { return `${v.title}\nUpload : ${v.date || 'unknown'} || Id : ${v.id}`}).filter(v => v).join('\n\n')
+	for (let x of res) arr.push({ title: x.title, description: `Upload : ${x.date || 'unknown'} || Id : ${x.id}`, rowId: `${_p}nekoinfo ${x.id}` })
+	try {
+	if (m.isGroup) return m.reply(tekss)
+	await conn.sendMessage(m.chat, { text: `Nekopoi Latest`, footer: null, title: null, buttonText: 'Click Here!', sections: [{ title: 'Nekopoi', rows: arr }] }, { quoted: m })
 	await apivisit
+	} catch (e) {
+		console.log(e)
+		m.reply(`Terjadi kesalahan atau server sedang mengalami gangguan.`)
+	}
 	// By Chandra XD
 	// Follow bang
 	// TikTok : @pnggilajacn
