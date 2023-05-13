e0430a8b498556916a00d05ad412bd0666cba810
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-13 23:11:36 +0700

    ^

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,25 +1,17 @@
-import { tmpdir } from 'os'
-import path, { join } from 'path'
-import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch
-} from 'fs'
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-m.reply('Succes !')
-const tmp = [tmpdir(), join(__dirname, '../tmp')]
-  const filename = []
-  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-  return filename.map(file => {
-    const stats = statSync(file)
-    unlinkSync(file)
-})
+import cp from 'child_process'
+import { promisify } from 'util'
+let exec = promisify(cp.exec).bind(cp)
+let handler = async (m) => {
+	await conn.reply(m.chat, "Done", m)
+    let o
+    try {
+        o = await exec('rm -rf tmp && mkdir tmp')
+    } catch (e) {
+        o = e
+    } 
 }
 handler.help = ['cleartmp']
 handler.tags = ['owner']
-handler.command = /^(cleartmp)$/i
-handler.rowner = true
+handler.command = /^(cleartmp|ctm)$/i
+handler.owner = true
 export default handler
