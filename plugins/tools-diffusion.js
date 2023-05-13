5cf3cff727fb185468a4d3d578b73493089ae7f4
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-13 23:15:43 +0700

    +

---
+++ plugins/tools-diffusion.js
@@ -0,0 +1,31 @@
+import fetch from "node-fetch"
+import { apivisit } from './kanghit.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Input Query\nExample : ${usedPrefix + command} 1girl, blush, looking to viewer, warm smile`
+    let randomS = randomId()
+    let res = await fetch(global.API('can', '/api/maker/diffusion', { text: text, seed: randomS }, 'apikey'))
+    let json = await res.json()
+    if(!json.base64Img) throw `Server Error 404`
+    let saveFilename = `./tmp/image.${json.ext}`;
+    let buffer = Buffer.from(json.base64Img, "base64");
+    try {
+    await conn.sendFile(m.chat, buffer, saveFilename, `Request from : `+ text, m)
+    await apivisit
+    } catch (e) {
+		console.log(e)
+		m.reply(`Terjadi kesalahan atau server sedang mengalami gangguan.`)
+	}
+	// By Chandra XD
+	// Follow bang
+	// TikTok : @pnggilajacn
+	// Github : https://github.com/Chandra-XD
+	}
+handler.help = ['diffusion'].map(v => v + ' <query>')
+handler.tags = ['tools']
+handler.command = /^(diffusion|ai-image)$/i
+export default handler
+
+function randomId() {
+	return Math.floor(100000000 + Math.random() * 100000000);
+}
