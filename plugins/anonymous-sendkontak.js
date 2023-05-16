aec712fd4f6da32ae261c44ee39e78bffcf8a615
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-16 07:50:18 +0700

    ^

--- plugins/anonymous-sendkontak.js
+++ plugins/anonymous-sendkontak.js
@@ -1,9 +1,9 @@
 let { MessageType, Presence } = (await import('@adiwajshing/baileys')).default
 async function handler(m, { command, conn, text }) {
-	conn.anonymous = conn.anonymous ? conn.anonymous : {}
+	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
-	if (!room) throw 'kamu tidak berada di anonymous chat'
+	if (!room) throw 'Kamu tidak berada di anonymous chat'
 	let other = room.other(who)
     var name
     if (text) name = text
@@ -11,9 +11,9 @@ async function handler(m, { command, conn, text }) {
 	var number = who.split('@')[0]
 	let tks = `➔ Nomor: ${m.sender.split`@`[0]}
 ➔ Nama: ${name}`
-        conn.reply(m.chat, 'Menggirimkan Kontak...', null)
-	if (other) conn.reply(other, `Partner mengirimkan kontak kepadamu`, null)
-	if (other) conn.sendMessage(other, { image: { url: await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png')}, caption: tks }, { quoted: m })
+    this.reply(m.chat, 'Menggirimkan Kontak...')
+	if (other) this.reply(other, `Partner mengirimkan kontak kepadamu`)
+	if (other) this.sendMessage(other, { image : { url: await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png')}, caption: tks })
 }
 handler.help = ['sendkontak']
 handler.tags = ['anonymous']
