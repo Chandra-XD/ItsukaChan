0230f83735a129f29da09c5ca1e9cb3f35bde138
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-16 07:57:26 +0700

    ^

--- plugins/misc-join.js
+++ plugins/misc-join.js
@@ -1,28 +1,19 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner }) => {
-    let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
-    if (!isOwner) {
-       m.reply('Link grup telah dikirimkan ke owner!\nHarap menunggu acc dari owner...')
-       for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
-        let data = (await conn.onWhatsApp(jid))[0] || {}
-        // conn.copyNForward(data.jid, m, true)
-        conn.sendButton(data.jid, `@${m.sender.split('@')[0]} Ingin menambahkan Botmu ke ${text}\nTerima/Tolak?`, 'Join Acc', null, [['Terima', `.join ${text}`], ['Tolak', '.']], m, { mentions: [m.sender] })
-      }
-       return
-    }
+    let [_, code, expired] = text.match(linkRegex) || []
+    if (!code) throw 'Link Salah'
     let res = await conn.groupAcceptInvite(code)
-    if (!res) throw res.toString()
-    let name = await conn.getName(res).catch(_ => null)
-    // expired =  Math.floor(Math.min(999, Math.max(1, isOwner ? expired && expired.isNumber() ? parseInt(expired) : 0 : 3)))
-    m.reply(`Berhasil join grup ${name || res}`)/*${expired ? `selama ${expired} hari` : ''}`)
-    
+    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
+    m.reply(`Berhasil Join Grup ${res}${expired ? ` Selama ${expired} Hari` : ''}`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-    */
 }
+handler.help = ['join'].map(v => v + ' <link>')
+handler.tags = ['owner']
 handler.command = /^join$/i
-// handler.rowner = true
+handler.owner = true
 export default handler
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
