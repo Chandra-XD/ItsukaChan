276d3bb778ea3a071a0ec1a8a6e7e550921602d2
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-16 08:13:32 +0700

    ^

--- plugins/owner-setpp.js
+++ plugins/owner-setpp.js
@@ -1,18 +1,65 @@
-import { webp2png } from '../lib/webp2mp4.js'
+import jimp_1 from 'jimp'
+import { URL_REGEX } from '@adiwajshing/baileys'
+import { apivisit } from './kanghit.js'
 
 let handler = async (m, { conn, args }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ''
-  if (/image/.test(mime)) {
-    let url = await webp2png(await q.download())
-    await conn.updateProfilePicture(conn.user.jid, { url }).then(_ => m.reply('Success update profile picture'))
-  } else if (args[0] && /https?:\/\//.test(args[0])) {
-    await conn.updateProfilePicture(conn.user.jid, { url: args[0] }).then(_ => m.reply('Success update profile picture'))
-  } else throw 'Where\'s the media?'
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+let botNumber = await conn.user.jid
+if (/image/g.test(mime) && !/webp/g.test(mime)) {
+let media = await q.download()
+let { img } = await pepe(media)
+			await conn.query({
+				tag: 'iq',
+				attrs: {
+					to: botNumber,
+					type:'set',
+					xmlns: 'w:profile:picture'
+				},
+				content: [
+					{
+						tag: 'picture',
+						attrs: { type: 'image' },
+						content: img
+					}
+				]
+			})
+			await m.reply(`Succes update profile bot`)
+			await apivisit
+} else if (args[0] && args[0].match(URL_REGEX)) {
+let { img } = await pepe(args[0])
+			await conn.query({
+				tag: 'iq',
+				attrs: {
+					to: botNumber,
+					type:'set',
+					xmlns: 'w:profile:picture'
+				},
+				content: [
+					{
+						tag: 'picture',
+						attrs: { type: 'image' },
+						content: img
+					}
+				]
+			})
+			await m.reply(`Succes update profile bot`)
+			await apivisit
+			} else throw 'Where\'s the media?'
 }
-handler.alias = ['setpp', 'setppbot']
+handler.alias = handler.help = ['setppbot']
+handler.tags = ['owner']
 handler.command = /^setpp(bot)?$/i
-
-handler.rowner = true
-
+handler.owner = true
 export default handler
+
+async function pepe(media) {
+	const jimp = await jimp_1.read(media)
+	const min = jimp.getWidth()
+	const max = jimp.getHeight()
+	const cropped = jimp.crop(0, 0, min, max)
+	return {
+		img: await cropped.scaleToFit(720, 720).getBufferAsync(jimp_1.MIME_JPEG),
+		preview: await cropped.normalize().getBufferAsync(jimp_1.MIME_JPEG)
+	}
+}
