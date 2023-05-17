1f896661d37edd1f9646478dd773afde5a272890
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-17 09:13:47 +0700

    ^

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,16 +1,18 @@
-import { facebookdlv2, savefrom } from '@bochilteam/scraper'
+import { savefrom } from '@bochilteam/scraper'
 import { apivisit } from './kanghit.js'
 
-let handler = async (m, { conn, args, usedPrefix: _p, command: cmd }) => {
+let handler = async (m, { conn, args, command }) => {
 	if (!args[0]) throw 'Input URL'
-	let res = await savefrom(args[0]).catch(async _ => await facebookdlv2(args[0])).catch(_ => null)
+	let res = await savefrom(args[0]).catch(_ => null)
 	if (!res) throw 'Error 404 Not Found'
 	await m.reply('Sedang diproses...')
-	let isHD = (res?.result?.length > 1 || res?.hd?.url) && !/^(?:-|--)hd$/i.test(args[1])
-//	if (isHD) return conn.sendButton(m.chat, res?.description || res?.meta?.title || ' ', wm, res?.result?.[1]?.url || res?.sd?.url, [['HD Quality', `${_p + cmd} ${args[0]} --hd`]], m)
-  	if (isHD) return conn.sendMessage(m.chat, { video: { url: res?.result?.[1]?.url || res?.sd?.url }, caption: res?.description || res?.meta?.title || ' '}, { quoted: m })
-	else await conn.sendMessage(m.chat, { video: { url: res?.result?.[0]?.url || res?.hd?.url || res?.url?.[0]?.url }, caption: res?.description || res?.meta?.title || '' }, { quoted: m })
+	try {
+  	await conn.sendMessage(m.chat, { video: { url: res?.hd?.url || res?.sd?.url }, caption: res?.meta?.title || ' '}, { quoted: m })
 	await apivisit
+	} catch (e) {
+		console.log(e)
+		m.reply(`Terjadi kesalahan.`)
+	}
 }
 handler.help = handler.alias = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['downloader']
