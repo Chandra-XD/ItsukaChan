922e6cf1334ec5693afbfdcb1b8c0838cd1dc373
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-05-17 22:43:55 +0700

    ^

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, command }) => {
 	if (!res) throw 'Error 404 Not Found'
 	await m.reply('Sedang diproses...')
 	try {
-  	await conn.sendMessage(m.chat, { video: { url: res?.hd?.url || res?.sd?.url }, caption: res?.meta?.title || ' '}, { quoted: m })
+  	await conn.sendMessage(m.chat, { video: { url: res?.hd?.url || res?.url?.[0]?.url || res?.sd?.url }, caption: res?.meta?.title || ' '}, { quoted: m })
 	await apivisit
 	} catch (e) {
 		console.log(e)
