881cb5a0d9963dd89d19a2d53b6d1fa06f39ec4b
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-06-16 18:57:13 +0700

    ^

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,44 +1,18 @@
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import FormData from 'form-data'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
 
 let handler = async (m) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = q.mediaType || ''
-	if (/image|video|audio|sticker|document/.test(mime)) {
-		let media = await q.download(true)
-		let data = await uploadFile(media)
-		m.reply(data.url)
-	} else throw 'No media found'
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'No media found'
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  m.reply(`Link : ${link}
+Size : ${media.length} Byte
+Expired : ${isTele ? 'No Expiry Date' : 'Unknown'}`)
 }
-handler.help = ['tourl']
+handler.help = ['upload (reply media)', 'tourl (reply media)']
 handler.tags = ['tools']
-handler.command = /^(upload|tourl)$/i
-
+handler.command = /^(tourl|upload)$/i
 export default handler
-
-async function uploadFile(path) {
-	let form = new FormData()
-	form.append('file', fs.createReadStream(path))
-	let res = await (await fetch('https://api.anonfiles.com/upload', {
-		method: 'post',
-		headers: {
-			...form.getHeaders()
-		},
-		body: form
-	})).json()
-	await fs.promises.unlink(path)
-	if (!res.status) throw res.error.message
-	let data = await fetch(res.data.file.url.full)
-	let $ = cheerio.load(await data.text())
-	return {
-		url: await shortUrl($('#download-url').attr('href')),
-		url2: res.data.file.url.short
-	}
-}
-
-async function shortUrl(url) {
-	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-	return await res.text()
-}
