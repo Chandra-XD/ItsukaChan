8658294bc0978b8423e7c3c2fe24fb605a0e16bb
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-10-27 07:35:49 +0700

    ^

--- plugins/misc-delete.js
+++ plugins/misc-delete.js
@@ -1,7 +1,14 @@
-function handler(m) {
-    if (m.quoted && m.quoted.fromMe) this.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}
-handler.alias = ['del', 'delete']
-handler.command = /^del(ete)?$/i
+let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+	if (m.quoted && m.quoted.fromMe) { // Pesan bot sendiri
+	await conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+	}
 
+    if (m.isGroup && isBotAdmin) { // Bot admin
+	await conn.sendMessage(m.chat, {
+		delete: {
+			...m.quoted.vM.key, participant: m.quoted.sender }
+		})}
+};
+handler.alias = ["del", "delete"];
+handler.command = /^del(ete)?$/i;
 export default handler
