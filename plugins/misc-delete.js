fc5d74b311a8aeee03435c78aed7bf86bbb246e6
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-10-27 07:45:38 +0700

    ^

--- plugins/misc-delete.js
+++ plugins/misc-delete.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+try {
 	if (m.quoted && m.quoted.fromMe) { // Pesan bot sendiri
 	await conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 	}
@@ -8,7 +9,10 @@ let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
 		delete: {
 			...m.quoted.vM.key, participant: m.quoted.sender }
 		})}
-};
+} catch (e) {
+throw "Reply message"
+}
+}
 handler.alias = ["del", "delete"];
 handler.command = /^del(ete)?$/i;
 export default handler
