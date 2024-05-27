d3096a17a282fc6c65e8b63c49d35a98ed3a3a1a
Muhammad Candra <115627115+Tjandraaaa@users.noreply.github.com>
2024-05-27 11:34:40 +0700

    -

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,28 +1,70 @@
-import yts from 'yt-search'
 import ytdl from 'ytdl-core'
-import { apivisit } from './kanghit.js'
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
+import search from 'yt-search'
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Input Query'
-  let vid = await yts(ytdl.validateURL(text) ? { videoId: await ytdl.getURLVideoID(text) } : { query: text })
-	vid = vid.videos ? vid.videos[0] : vid
-  if (!vid) throw 'Video/Audio Tidak Ditemukan'
+  conn.sendMessage(m.chat, { react: { text: `🕑`, key: m.key }})
+  try {
+    let results = await search(text);
+    let videoId = results.videos[0].videoId;
+    let info = await ytdl.getInfo(videoId);
+    let title = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+    let thumbnailUrl = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+    let url = info.videoDetails.video_url;
+    let duration = parseInt(info.videoDetails.lengthSeconds);
+    let uploadDate = new Date(info.videoDetails.publishDate).toLocaleDateString();
+    let views = info.videoDetails.viewCount;
+    let minutes = Math.floor(duration / 60);
+    let description = results.videos[0].description;
+    let seconds = duration % 60;
+    let durationText = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;       
+    let audio = ytdl(videoId, { quality: 'highestaudio' });
+    let inputFilePath = './tmp/' + title + '.webm';
+    let outputFilePath = './tmp/' + title + '.mp3';
+    let viewsFormatted = formatViews(views);
+    let caption = `*Title*: ${title}\n*Duration*: ${durationText}\n*Upload*: ${uploadDate}\n*Views*: ${viewsFormatted}\n*ID*: ${videoId}\n*Description*: ${description}\n*URL*: ${url}
+  `;
+    const pesan = conn.sendMessage(m.chat, { image: { url: thumbnailUrl }, caption }, { quoted: m})
 
-  let { title, description, url, seconds, timestamp, views, ago, thumbnail } = vid
-  
-  let capt = `*Title:* ${title}\n*Published:* ${ago}\n*Views:* ${views}\n*Description:* ${description}\n*Url:* ${url}`
-  
-  let ytthumb = await (await conn.getFile(thumbnail)).data
-  let repl = await conn.reply(m.chat, capt, m, { contextInfo: { externalAdReply :{ mediaUrl: url, mediaType: 1, description: null, title: title, body: description, renderLargerThumbnail: true, thumbnail: ytthumb, sourceUrl: url }}})
+    audio.pipe(fs.createWriteStream(inputFilePath)).on('finish', async () => {
+      ffmpeg(inputFilePath)
+        .toFormat('mp3')
+        .on('end', async () => {
+          let buffer = fs.readFileSync(outputFilePath);                    
+          conn.sendMessage(m.chat, {         
+                audio: buffer,
+                mimetype: 'audio/mpeg'
+            }, { quoted: m}
+          fs.unlinkSync(inputFilePath);
+          fs.unlinkSync(outputFilePath);
+        })
+        .on('error', (err) => {
+          console.log(err);
+          m.reply(`There was an error converting the audio: ${err.message}`);
+          fs.unlinkSync(inputFilePath);
+          fs.unlinkSync(outputFilePath);
+        })
+        .save(outputFilePath);
+    });
+  } catch (e) {
+    console.log(e);
+    m.reply(`An error occurred while searching for the song: ${e.message}`);
+  }
+};
 
-  await conn.sendMessage(m.chat, { [seconds > 1900 ? 'document' : 'audio']: { url: `https://popcat.xyz/download?url=`+url+`&filter=audio&filename=temp` }, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: repl })
-  await apivisit
-	// By Chandra XD
-	// Follow bang
-	// TikTok : @pnggilajacn
-	// Github : https://github.com/Chandra-XD
-}
 handler.help = handler.alias = ['play'].map(v => v + ' <query>')
 handler.tags = ['downloader']
 handler.command = /^(play)$/i
 export default handler
+
+function formatViews(views) {
+  if (views >= 1000000) {
+    return (views / 1000000).toFixed(1) + 'M';
+  } else if (views >= 1000) {
+    return (views / 1000).toFixed(1) + 'K';
+  } else {
+    return views.toString();
+  }
+	    
